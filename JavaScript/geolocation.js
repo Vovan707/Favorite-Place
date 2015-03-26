9b048bab84da5d440eeed92d15a01665667a9295
Vovan707 <krokhmalyuk777@gmail.com>
2015-03-26 18:10:22 +0200

    Corect code

--- JavaScript/geolocation.js
+++ JavaScript/geolocation.js
@@ -2,7 +2,7 @@ var GeoLocation = {
   getAdress: function(atr) {
     var city = document.getElementById(atr).getAttribute('data-city'),
       street = document.getElementById(atr).getAttribute('data-street'),
-	  streetNumber = document.getElementById(atr).getAttribute('data-street'),
+	  streetNumber = document.getElementById(atr).getAttribute('data-street-number'),
 	  zipCode = document.getElementById(atr).getAttribute('data-zip-code'),
       adress = city + " " + street+" "+streetNumber+" "+zipCode
     return adress
@@ -16,8 +16,9 @@ var GeoLocation = {
 }
 
 GoToPage = {
-  goToUrl: function(atr, id) {
-    if (id === "adress") {
+  goToUrl: function(atr) {
+	  var d = document.getElementById(atr); 
+    if (d.hasAttribute("data-city")) {
       window.open("http://maps.google.com?q=" + GeoLocation.getAdress(atr))
     } else {
       window.open("http://maps.google.com?q=" + GeoLocation.getCoordinats(atr))
