641d1e472352fcdee6a66668848040bae394695b
Volodymyr Krokhmalyuk <krokhmalyuk777@mail.ru>
2015-03-26 22:57:47 +0200

    Second formated

--- JavaScript/geolocations.js
+++ JavaScript/geolocations.js
@@ -2,12 +2,12 @@ var GeoLocation = {
   getCoordinates: function(atr) {
     var id = document.getElementById(atr);
     if (id.hasAttribute("data-latitude")) {
-      latitude = document.getElementById(atr).getAttribute('data-latitude'),
+        latitude = document.getElementById(atr).getAttribute('data-latitude'),
         longitude = document.getElementById(atr).getAttribute('data-longitude'),
         coordinats = latitude + "," + longitude;
       return coordinats;
     } else {
-      var city = document.getElementById(atr).getAttribute('data-city'),
+    var city = document.getElementById(atr).getAttribute('data-city'),
         street = document.getElementById(atr).getAttribute('data-street'),
         streetNumber = document.getElementById(atr).getAttribute('data-street-number'),
         zipCode = document.getElementById(atr).getAttribute('data-zip-code'),
